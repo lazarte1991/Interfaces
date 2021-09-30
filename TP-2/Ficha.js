e305068bab1b02f6a8e26330ed122248c326b31c
Rocio Poppe <rociocpoppe@gmail.com>
2021-09-29 23:17:03 -0300

    agrego arreglo de fichas en main y lo cargo. falta hacerque sea dinamico y segun ficha elegida

--- TP-2/Ficha.js
+++ TP-2/Ficha.js
@@ -2,16 +2,22 @@ class Ficha{
 
     constructor(ctx){
         this.ctx=ctx;
+        //this.fichas=[];
     }
-    
 
     cargarImagen (){
         let imageObj = new Image();
         imageObj.src = 'img/1.jpeg';
         imageObj.onload = function () {
-            //falta redimensionar imagen
-            ctx.drawImage( imageObj, 0, 0 );
-            
+            ctx.drawImage( imageObj, 0, 0 ,75,75); 
         }
-    };
+    }
+
+   /* agregarFicha(){
+        this.fichas.push(ficha);
+    }
+
+    eliminarFicha(){
+        this.fichas.pop(ficha);
+    }*/
 }
