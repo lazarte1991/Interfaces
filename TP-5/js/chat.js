906304add2760f25b1d36e3095a435354d063982
Leandro <lazarteleandro5@gmail.com>
2021-12-02 00:20:53 -0300

    chat terminado

---
+++ TP-5/js/chat.js
@@ -0,0 +1,27 @@
+let txtArea = document.getElementById("txt-area");
+let cuerpoChat = document.getElementById("cuerpo-chat");
+
+
+document.addEventListener('keydown', chekTextArea);
+document.addEventListener('keyup', chekTextArea);
+document.getElementById("send").addEventListener("click", enviar);
+
+function chekTextArea() {
+    if (txtArea.value != "") {
+        document.getElementById("send").src = "https://img.icons8.com/ios-glyphs/35/975FCE/paper-plane.png";
+    } else {
+        document.getElementById("send").src = "https://img.icons8.com/material-rounded/35/000000/microphone.png";
+    }
+
+}
+
+function enviar() {
+
+    if (txtArea.value != "") {
+        cuerpoChat.innerHTML += `<div class="msj-propio">
+        <p>${txtArea.value}</p>
+        <p class="hora">00:23</p>
+        </div>`;
+    }
+
+}
