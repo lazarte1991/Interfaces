074725a743565633d0420787df03e96a9dff8edc
Rocio Poppe <rociocpoppe@gmail.com>
2021-12-02 19:56:23 -0300

    agrego principio buscador

--- TP-5/js/muro.js
+++ TP-5/js/muro.js
@@ -2,21 +2,13 @@
 
 let btnLike=document.getElementById("botonLike").addEventListener("click", actualizar);
 
-// let btnComentar=document.getElementById("botonComentar").addEventListener("click", comentar);
-
-// let btnCompartir=document.getElementById("botonCompartir").addEventListener("click", compartir);
-
-
 var valor = 15;
-// let valor2=2954;
-// let valor3=148;
+
 let like=false;
 
 
 function actualizar() {
     let contador=document.getElementById("contadorLikes").innerHTML = valor + " likes";
-    // let contador1=document.getElementById("contadorLikes1").innerHTML = valor2 + " likes";
-    // let contador2=document.getElementById("contadorLikes2").innerHTML = valor3 + " likes";
     if(!like){
         document.getElementById("like").src="https://img.icons8.com/ios-filled/20/fa314a/like--v1.png"; 
         like=true;
@@ -25,32 +17,14 @@ function actualizar() {
         document.getElementById("like").src="https://img.icons8.com/ios-filled/20/000000/filled-like.png";
         like=false;
         valor++;
-    }
-
-
-    
+    }    
 }
-
-//  let contenedor=document.getElementByClass("mensajeBuscador");
-// function buscar ( ) {
-
-    
-//     let str = document.getElementById("busqueda").value;
-   
-//     if(str=="g"){
-//        contenedor.innerHTML+="Debe ingresar caracteres";
-
-//     }
-    
-//   }
   
-
-  let btnComentarios=document.getElementById("botonComentar").addEventListener("click", actualizarComentarios);
+let btnComentarios=document.getElementById("botonComentar").addEventListener("click", actualizarComentarios);
   
 let valorComentarios=29;
 
 function actualizarComentarios(){
-
     let div=document.querySelectorAll("div");
     for(let di of div){
         di.classList.replace("inputComentarios", "mostrar");  
@@ -61,13 +35,37 @@ function actualizarComentarios(){
 }
 
 function borrarComentarios(){
-   
-   
-        valorComentarios++;
-        document.getElementById("contadorComentarios").innerHTML=valorComentarios + " comentarios"; 
-        let div=document.querySelectorAll("div");
+    valorComentarios++;
+    document.getElementById("contadorComentarios").innerHTML=valorComentarios + " comentarios"; 
+    let div=document.querySelectorAll("div");
     for(let di of div){
         di.classList.replace("mostrar", "inputComentarios");  
     }
+}
+
+
+document.querySelector("mensajeBuscador");
+ 
+let arrayPublicaciones=[];
+function buscar ( ) {
+    let str = document.getElementById("busqueda").value;
+    let div=document.querySelectorAll("div");
+    for(let di of div){
+        di.classList.replace("muroCentral", "mensajeBuscador");  
+    }
+    if(str==""){
+        document.getElementById("mensajeBusqueda").innerHTML="Debe ingresar caracteres";
+    }
+    else{
+        let arrayContactos=["María Garcia", "Juan Gonzalez", "Natalia Gimenez", "Ricardo Perez", "Matías Gonzalez"];
+        for(let contacto of arrayContactos){
+            if(arrayContactos.includes(str)){
+                document.getElementById("mensajeBusqueda").innerHTML="esta el contacto";
+            }
+            else{
+                document.getElementById("mensajeBusqueda").innerHTML="esta persona aún no está registrada";
+            }
+        }
     
+    }
 }
