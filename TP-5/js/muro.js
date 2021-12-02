d3725fb88c37a7286847afdde30404faa0d990db
Rocio Poppe <rociocpoppe@gmail.com>
2021-12-01 21:27:25 -0300

    agrego funcionalidad like/dislike y barra cantidad reacciones

--- TP-5/js/muro.js
+++ TP-5/js/muro.js
@@ -1,18 +1,34 @@
 
 
-let btnLike=document.getElementById("botonLike").addEventListener("click", contar);
+let btnLike=document.getElementById("botonLike").addEventListener("click", actualizar);
 
-let btnComentar=document.getElementById("botonComentar").addEventListener("click", comentar);
+// let btnComentar=document.getElementById("botonComentar").addEventListener("click", comentar);
 
-let btnCompartir=document.getElementById("botonCompartir").addEventListener("click", compartir);
+// let btnCompartir=document.getElementById("botonCompartir").addEventListener("click", compartir);
 
 
-var valor = 0;
+var valor = 14;
+let like=false;
+
 
 function contar(){
-    updateDisplay(valor++);
+   
 }
 
-function updateDisplay(val) {
-    document.getElementById("contadorLikes").innerHTML = val;
+function actualizar() {
+    document.getElementById("contadorLikes").innerHTML = valor + " likes";
+    if(!like){
+        document.getElementById("like").src="https://img.icons8.com/ios-filled/20/fa314a/like--v1.png"; 
+        like=true;
+        valor--;
+    }else{
+        document.getElementById("like").src="https://img.icons8.com/ios-filled/20/000000/filled-like.png";
+        like=false;
+        valor++;
+
+    }
+
+
+    
 }
+
