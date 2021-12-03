600b34f0fe354744b3176f5f378b825649fe0f0c
Rocio Poppe <rociocpoppe@gmail.com>
2021-12-02 22:28:13 -0300

    agrego buscador

--- TP-5/js/muro.js
+++ TP-5/js/muro.js
@@ -46,26 +46,143 @@ function borrarComentarios(){
 
 document.querySelector("mensajeBuscador");
  
-let arrayPublicaciones=[];
-function buscar ( ) {
+
+
+let contactos=[{
+    "nombre":"María Garcia",
+    "ficha":
+        `<div class="fichaContacto">
+                    <img src="./img/chica6.jpeg"/>
+                    <h3>María Garcia</h3>
+        </div>`,
+    "publicacion": `<div class="publicacion">
+                        <div class="info">
+                            <img src="./img/chica6.jpeg" alt="">
+                            <h3>María Garcia</h3>
+                            <!-- <h5>2 Hs</h5>  -->
+                        </div>
+                        <div class="imagenPublicada">
+                            <img src="./img/gato.jpeg" alt="" >
+                        </div>
+                        <div class="contador">
+                            <h5 id="contadorLikes2"> 148 likes</h5>
+                            <h5 id="contadorComentarios2"> 245 comentarios</h5>
+                            <h5 id="contadorCompartidos2"> 162 compartido</h5>
+                        </div>
+                        <div class="reacciones">
+                            <button id="botonLike2">
+                                <img src="https://img.icons8.com/ios-filled/20/000000/filled-like.png" id="like2"/>like
+                            </button> 
+                            <button id="botonComentar2"><img src="https://img.icons8.com/ios-filled/20/000000/speech-bubble-with-dots.png"/>comentar</button>
+                            <button id="botonCompartir2"><img src="https://img.icons8.com/ios-filled/20/000000/forward-arrow.png"/>compartir</button>
+                        </div>
+                        <div class="inputComentarios2">           
+                            <input id="comentariosTerceros2" cols="20" rows="8" placeholder="Escribe un mensaje aquí" onkeyup = "if(event.keyCode == 13)borrarComentarios2()"></input> 
+                        </div>
+                    
+                    </div> `
+},
+  { "nombre": "Matías Gonzalez",
+    "ficha":    `<div class="fichaContactoBusqueda">
+                    <div>
+                        <img src="./img/chio8.jpg"/>
+                        <h3>Matías Gonzalez</h3>
+                    </div>
+                    <button>Eliminar contacto"</button>
+                </div>`,
+    "publicacion": `<div class="publicacion">
+                                    <div class="info">
+                                        <img src="./img/chio8.jpg"/>
+                                        <h3>Matías Gonzalez</h3>
+                                        <!-- <h5>2 Hs</h5>  -->
+                                    </div>
+                                    <div class="imagenPublicada">
+                                        <img src="./img/perro-labrador-portada.jpg" alt="" >
+                                    </div>
+                                    <div class="contador">
+                                        <h5 id="contadorLikes"> 14 likes</h5>
+                                        <h5 id="contadorComentarios"> 29 comentarios</h5>
+                                        <h5 id="contadorCompartidos"> 2 compartido</h5>
+                                    </div>
+                                    <div class="reacciones">
+                                        <button id="botonLike">
+                                            <img src="https://img.icons8.com/ios-filled/20/000000/filled-like.png" id="like"/>like
+                                        </button> 
+                                        <button id="botonComentar"><img src="https://img.icons8.com/ios-filled/20/000000/speech-bubble-with-dots.png"/>comentar</button>
+                                        <button id="botonCompartir"><img src="https://img.icons8.com/ios-filled/20/000000/forward-arrow.png"/>compartir</button>
+                                    </div>
+                                    <div class="inputComentarios">           
+                                        <input id="comentariosTerceros" cols="20" rows="8" placeholder="Escribe un mensaje aquí" onkeyup = "if(event.keyCode == 13)borrarComentarios()"></input> 
+                                    </div>
+                                </div>`,
+        
+
+    
+  },{
+      "nombre": "Natalia Gimenez",
+      "ficha":`<div class="fichaContacto">
+                    <img src="./img/chica9.jpeg">
+                    <h3>Natalia Gimenez</h3>
+                </div>`,
+      "publicacion":`<div class="publicacion">
+                <div class="info">
+                    <img src="./img/chica9.jpeg">
+                    <h3>Natalia Gimenez</h3> 
+                    <div>
+                    <!-- <h5>2 Hs</h5>   -->
+                    </div>    
+                </div>
+                <div class="imagenPublicada">
+                    <img src="./img/perro-labrador-portada.jpg" alt="" >
+                </div>
+                <div class="contador">
+                    <h5 id="contadorLikes1"> 2954 likes</h5>
+                    <h5 id="contadorComentarios1"> 739 comentarios</h5>
+                    <h5 id="contadorCompartidos1"> 503 compartido</h5>
+                </div>
+                <div class="reacciones">
+                    <button id="botonLike1">
+                        <img src="https://img.icons8.com/ios-filled/20/000000/filled-like.png" id="like1"/>like
+                    </button> 
+                    <button id="botonComentar1"><img src="https://img.icons8.com/ios-filled/20/000000/speech-bubble-with-dots.png"/>comentar</button>
+                    <button id="botonCompartir1"><img src="https://img.icons8.com/ios-filled/20/000000/forward-arrow.png"/>compartir</button>
+                </div>
+                <div class="inputComentarios">           
+                    <input id="comentariosTerceros1" cols="20" rows="8" placeholder="Escribe un mensaje aquí" onkeyup = "if(event.keyCode == 13)borrarComentarios1()"></input> 
+                </div>
+            </div>`
+  },{
+    "nombre":"Ricardo Perez",
+    "ficha":`<div class="fichaContacto">
+                <img src="./img/hombre7.jpeg"/>
+                <h3>Ricardo Perez </h3>
+            </div>`
+  }
+];
+    
+
+function buscar () {
     let str = document.getElementById("busqueda").value;
     let div=document.querySelectorAll("div");
     for(let di of div){
         di.classList.replace("muroCentral", "mensajeBuscador");  
     }
     if(str==""){
-        document.getElementById("mensajeBusqueda").innerHTML="Debe ingresar caracteres";
+        document.querySelector(".mensajeBuscador").innerHTML='<h2>Debe ingresar caracteres</h2>';
     }
     else{
-        let arrayContactos=["María Garcia", "Juan Gonzalez", "Natalia Gimenez", "Ricardo Perez", "Matías Gonzalez"];
-        for(let contacto of arrayContactos){
-            if(arrayContactos.includes(str)){
-                document.getElementById("mensajeBusqueda").innerHTML="esta el contacto";
+    
+        for(let contacto of contactos){
+            if(contacto.nombre.includes(str)){
+                console.log(contacto.ficha);
+                document.querySelector(".mensajeBuscador").innerHTML=`${contacto.ficha}`;
+                if(contacto.publicacion!=undefined)
+                document.querySelector(".mensajeBuscador").innerHTML+=`${contacto.publicacion}`;
             }
             else{
-                document.getElementById("mensajeBusqueda").innerHTML="esta persona aún no está registrada";
+                document.querySelector(".mensajeBuscador").innerHTML='<h2>No hay resultados</h2>';
             }
         }
-    
     }
+    
 }
